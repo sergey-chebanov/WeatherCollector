20400a9867758c4a26ba3d085d0eb1700c6fb9fd
Sergey Chebanov <sergey.chebanov@gmail.com>
2016-04-07 00:30:01 +0300

    Try to use python3 and pyserial 3
    Failed

--- WeatherStation/sensor-listener.py
+++ WeatherStation/sensor-listener.py
@@ -1,20 +1,23 @@
+import sys
+import time
+import traceback
 from sensor import getMeasurments
 from measurmentsDB import addMeasurment
 import logging as log
 log.basicConfig(format='%(asctime)s %(message)s', filename='sensor.log', level=log.DEBUG)
 
 if __name__ == '__main__':
-    try:
-        measurments = getMeasurments()
-    except:
-        log.debug('first try failed...')
-
-    if measurments is None:
+    for x in range(3):
         try:
+            log.debug('try {} ...'.format(x))
             measurments = getMeasurments()
+            if measurments is not None:
+                log.debug('try {} succeded: {}'.format(x, measurments))
+                addMeasurment(*measurments)
+                break
+            else:
+                time.sleep(5*2**x)
         except:
-            log.debug('second try failed...')
-
-    if measurments is not None:
-        log.info('measurments: {0}'.format(measurments))
-        addMeasurment(*measurments)
+            traceback.print_exc()
+            log.error("{}".format(sys.exc_info()))
+        log.debug('try {} failed...'.format(x))
--- WeatherStation/sensor.py
+++ WeatherStation/sensor.py
@@ -1,29 +1,39 @@
 import serial
+import sys
 import time
 import re
-
+import logging as log
+log.basicConfig(format='%(asctime)s %(message)s', filename='sensor.log', level=log.DEBUG)
 
 def command(cmd):
-	ser = serial.Serial('/dev/rfcomm0', baudrate=115200)
-	ser.timeout = 2
-	ser.write(cmd)
-	ser.flush()
-	result = []
-	time.sleep(1)
-	while ser.inWaiting() > 0:
-		s = ser.readline()
-		result.append (s.strip())
-		time.sleep(0.1)
-
-	return result
+    with serial.Serial('/dev/rfcomm0', baudrate=9600, timeout=2) as ser:
+        ser.flush()
+        ser.write(cmd)
+        ser.flush()
+        result = []
+        time.sleep(1)
+        #log.debug('inWaiting {}'.format(ser.inWaiting()))
+        while ser.inWaiting() > 0:
+            s = ser.readline()
+            log.debug('{}'.format(s))
+            result.append (s.strip())
+            time.sleep(0.1)
+        return result
 
 def getMeasurments():
-	rawMeasurments = "".join(command ('m'))
-	m = re.match("(.*):(.*):(.*):(.*):(.*)", rawMeasurments)
-	if m is not None:
-		return (m.group(1), m.group(2), m.group(5))
-	else:
-		return None
+    rawMeasurments = ''.join(command ('m'))
+    log.debug('raw input: {}'.format(rawMeasurments))
+    try:
+        temp, hum, temp2, _, pres = rawMeasurments.split(':')
+        temp = (float(temp2) + float(temp)) / 2
+    except ValueError:
+        return None
+    except:
+        log.error("{}".format(sys.exc_info()))
+        raise
+    else:
+        log.debug('{} {} {}'.format(temp, hum, pres))
+        return (temp, float(hum), float(pres))
 
 if __name__ == '__main__':
-	print(command('t'))
+    print(command('t'))
